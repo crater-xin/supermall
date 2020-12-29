7ee686b942840a222cf9624d4232edfbdae41db3
辛冬峰 <1813059208@qq.com>
2020-12-29 14:57:59 +0800

    滚动图,推荐两个模块

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,47 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+
+import { getHomeMultidata } from 'network/home'
+
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then(res => {
+      // console.log(res);
+      // es6箭头函数的this,在定义时就继承了定义函数的对象
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
+  }
 }
 
 </script>
 <style>
+.home-nav{
+  background-color: var(--color-tint);
+  color: #ffffff;
+}
 </style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item, key) in recommends" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecommendView',
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+
+</script>
+<style>
+.recommend{
+  display: flex;
+  text-align: center;
+  font-size: 12px;
+
+  padding: 15px 0 20px 0;
+  border-bottom: 10px solid #eee;
+}
+
+.recommend-item{
+  flex: 1;
+}
+
+.recommend-item img{
+  width: 68px;
+  height: 68px;
+  margin-bottom: 10px;
+}
+</style>
